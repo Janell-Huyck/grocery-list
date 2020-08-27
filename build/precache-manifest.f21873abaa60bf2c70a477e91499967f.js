7fb73bd3a0b2c566fb6f4fc080e92877d454f04b
Janell.Huyck <Janell.Huyck@outlook.com>
2020-08-27 17:58:14 -0400

    ran 'npm run deploy'

---
+++ build/precache-manifest.f21873abaa60bf2c70a477e91499967f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed359d4c1df0ffe0816e803498a0f905",
+    "url": "/grocery-list/index.html"
+  },
+  {
+    "revision": "813e5cc94a4aca43dc5c",
+    "url": "/grocery-list/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "61febb0d8a41dea72d6d",
+    "url": "/grocery-list/static/css/main.173c26ab.chunk.css"
+  },
+  {
+    "revision": "813e5cc94a4aca43dc5c",
+    "url": "/grocery-list/static/js/2.61bf7da9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/grocery-list/static/js/2.61bf7da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61febb0d8a41dea72d6d",
+    "url": "/grocery-list/static/js/main.b5f07433.chunk.js"
+  },
+  {
+    "revision": "3f79d6f9011e342088c4",
+    "url": "/grocery-list/static/js/runtime-main.a83510f9.js"
+  }
+]);
